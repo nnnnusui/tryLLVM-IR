914589f036e0f025a4413d2b375c18750a2cb102
nnnnusui <nnnnusui@gmail.com>
2020-11-03 14:33:38 +0900

    :pencil2: Supports addition and subtraction

--- main.c
+++ main.c
@@ -7,8 +7,32 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   printf("define i32 @main() {\n");
-  printf("  ret i32 %d\n", atoi(argv[1]));
+  printf("  %%1 = add i32 %ld, 0\n", strtol(p, &p, 10));
+  
+  int index = 0;
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      index++;
+      printf("  %%%d = add i32 %%%d, %ld\n", index + 1, index, strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      index++;
+      printf("  %%%d = sub i32 %%%d, %ld\n", index + 1, index, strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    return 1;
+  }
+
+  printf("  ret i32 %%%d\n", index + 1);
   printf("}\n");
   return 0;
 }
